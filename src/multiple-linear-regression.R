7aecf9c90a23123d325a66cd92afab4672ad6122
adebesing <gregory.adebesin@gmail.com>
2017-01-07 18:00:40 +0000

    two models

---
+++ src/multiple-linear-regression.R
@@ -0,0 +1,53 @@
+# avg_time is the response (what we are trying to predict) distance is the predictor (what we are using to predict the response)
+#fit <- lm(formula = avg_time ~ distance , data =  rs)
+library(car)
+
+data("Prestige")
+
+summary(Prestige)
+
+na_omit_prestige <- na.omit(Prestige) 
+
+#Numberof observations
+num_obs <- nrow(na_omit_prestige)
+
+#60% for training set 
+num_train <- round(num_obs*0.6)
+
+#Set seed for reproducible results
+set.seed(333)
+
+#Create an index
+indx <- sample(num_obs, num_train)
+
+train_prestige <- na_omit_prestige[indx,]
+test_prestige <- na_omit_prestige[-indx,]
+
+# Some quick exploratory data analysis on training set by plotting the response variable
+# in our case here (prestige) against several other predictors to check for linear 
+# relationships
+
+#No trend
+plot(train_prestige$prestige, train_prestige$income)
+
+#Trend 
+plot(train_prestige$prestige, train_prestige$education)
+
+#No trend
+plot(train_prestige$prestige, train_prestige$women)
+
+# ~ . means to use all of the other variables as predictors
+fit <- lm(formula = prestige ~ ., data = train_prestige)
+
+# Features not marked with at least one asterisk can be safely ignored.
+summary(fit)
+
+plot(fit$fitted, fit$residuals)
+
+prediction <- predict(fit, newdata = test_prestige)
+
+predict(fit, data.frame(education=c(100, 200, 300), income=c(13,45,66), women=c(11,22,3), census=c(111,2,4), type=c("prof","prof","prof")))
+
+cor(prediction, test_prestige$prestige)
+
+
---
+++ src/simple-linear-regression.R
@@ -0,0 +1,27 @@
+library(MASS)
+data("Boston")
+
+names(Boston)
+
+fit <- lm(formula = medv ~ rm, data = Boston)
+
+summary(fit)
+
+attach(Boston)
+
+plot(rm, medv, pch=20, xlab="Avg Rooms", ylab="Median Value")
+
+lines(rm, fit$fitted, lwd=3)
+
+# Let’s say you want to predict the median home value based on 
+# the average number of rooms being — 2, 4 and 5
+
+coef(fit)[1] + coef(fit)[2] * 2
+coef(fit)[1] + coef(fit)[2] * 4
+coef(fit)[1] + coef(fit)[2] * 5
+
+# Or using predict...
+
+predict(fit, data.frame(rm = c(2, 4, 5)))
+
+
